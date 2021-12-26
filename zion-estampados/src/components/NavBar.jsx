4fdb50174ba85043acfe5d7a94b00de16a7b368f
Destinycod <clara.rodriguez.a@hotmail.com>
2021-12-26 16:16:57 -0300

    prueba

--- zion-estampados/src/components/NavBar.jsx
+++ zion-estampados/src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-bootstrap/lib/Navbar';
 import Navbar from 'react-bootstrap/Navbar'
 
 function NavBar(){
@@ -7,7 +8,7 @@ function NavBar(){
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    
+                    <Link>Uno</Link>
                     <Nav.Link href="#link">Link</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
